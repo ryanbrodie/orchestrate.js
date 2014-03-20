74074d94d6481cbfaf10a7c8bf1730af1a63808f
Steve Kaliski <sjkaliski@gmail.com>
2014-03-20 15:59:22 -0400

    support multiple related parameters, closes #25

--- test/graph.test.js
+++ test/graph.test.js
@@ -17,6 +17,13 @@ var movies = {
   }
 }
 
+var friends = {
+  david: {
+    "name": "David Byrd",
+    "age": 20
+  }
+}
+
 // Override http requests.
 var fakeOrchestrate = nock('https://api.orchestrate.io')
   .get('/v0/users/sjkaliski%40gmail.com/relations/likes')
@@ -36,6 +43,23 @@ var fakeOrchestrate = nock('https://api.orchestrate.io')
     ],
     "count": 1
   })
+  .get('/v0/users/sjkaliski%40gmail.com/relations/friends/friends')
+  .reply(200, {
+    "results": [
+      {
+        "path": {
+          "collection": "friends",
+          "key": "david@bowery.io",
+          "ref": "56e22c26346f9016"
+        },
+        "value": {
+          "name": "David Byrd",
+          "age": 20
+        }
+      }
+    ],
+    "count": 1
+  })
   .put('/v0/users/sjkaliski%40gmail.com/relation/likes/movies/Superbad')
   .reply(204)
   .delete('/v0/users/sjkaliski%40gmail.com/relation/likes/movies/Superbad?purge=true')
@@ -49,6 +73,17 @@ suite('Graph', function () {
     .related('likes')
     .then(function (res) {
       assert.deepEqual(res.body.results[0].value, movies.superbad)
+      done()
+    })
+  })
+
+  test('Get distant graph relationship', function (done) {
+    db.newGraphReader()
+    .get()
+    .from('users', 'sjkaliski@gmail.com')
+    .related('friends', 'friends')
+    .then(function (res) {
+      assert.deepEqual(res.body.results[0].value, friends.david)
       done()
     })
   })
